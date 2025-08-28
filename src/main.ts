91082d9c34515cf6175a15af7559b4c25aa74ea6
briandg-dev <briandg.dev@gmail.com>
2025-08-28 12:33:10 -0300

    Habilitacion de cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();//Habilita cors para el front
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
